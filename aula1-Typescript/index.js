ae62a2e453fafb23312fcaaf437017d759f69206
FerShibli <fernandoluiz.1991@gmail.com>
2024-08-15 22:16:45 -0300

    feat(aula1): index and promptSync

---
+++ aula1-Typescript/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const promptSync = require("prompt-sync");
+var prompt = promptSync();
+const b = prompt('insira o valor para b >>> ');
+console.log(`o valor digitado foi: ${b}`);
+const c = 'halalala umanamana';
+const d = 'terceira linha';
---
+++ aula1-Typescript/index.ts
@@ -0,0 +1,9 @@
+import promptSync = require('prompt-sync')
+
+var prompt = promptSync()
+
+const b: string = prompt('insira o valor para b >>> ')
+console.log(`o valor digitado foi: ${b}`)
+const c: string = 'halalala umanamana'
+const d: string = 'terceira linha'
+
