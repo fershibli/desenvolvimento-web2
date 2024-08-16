144749ecc4198aebfb467ee2b43302b410f686b0
FerShibli <fernandoluiz.1991@gmail.com>
2024-08-15 22:16:02 -0300

    feat(aula1): meuPrimeiroTs

---
+++ aula1-Typescript/meuPrimeiroTs.js
@@ -0,0 +1,6 @@
+"use strict";
+const mensagem = 'Hello World!';
+console.log(mensagem);
+var languages = [];
+languages.push('typescript');
+languages.push(3);
---
+++ aula1-Typescript/meuPrimeiroTs.ts
@@ -0,0 +1,8 @@
+const mensagem : string = 'Hello World!'
+
+console.log(mensagem)
+
+var languages : Array<number|string> = []
+
+languages.push('typescript')
+languages.push(3)
