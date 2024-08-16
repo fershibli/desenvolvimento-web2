438733b1cb310506e9fbc5e35b9a62ad41ff845b
FerShibli <fernandoluiz.1991@gmail.com>
2024-08-15 22:18:38 -0300

    feat(aula1): types

---
+++ aula1-Typescript/types.js
@@ -0,0 +1,15 @@
+"use strict";
+// sintaxe
+// nomedavariavel : tipo
+// string, boolean, number
+// string = caracteres alfanuméricos "", '', ``
+const nome = 'Janaina';
+const sobrenome = 'Ferreira';
+const nomeCompleto = nome + " " + sobrenome;
+const nomeCompleto2 = `nome: ${nome} \nsobrenome: ${sobrenome}`;
+//testando o tipo string
+const numero1 = '10';
+const isLoading = true; //true ou false
+const numeros = 10; //inteiro, floats, doubles, hexadecimal, octal
+console.log(nomeCompleto);
+console.log(nomeCompleto2);
---
+++ aula1-Typescript/types.ts
@@ -0,0 +1,19 @@
+// sintaxe
+// nomedavariavel : tipo
+// string, boolean, number
+// string = caracteres alfanuméricos "", '', ``
+const nome : string = 'Janaina'
+const sobrenome : string = 'Ferreira'
+
+const nomeCompleto : string = nome + " " + sobrenome
+const nomeCompleto2 : string = `nome: ${nome} \nsobrenome: ${sobrenome}`
+
+//testando o tipo string
+const numero1 : string = '10'
+
+const isLoading : boolean =  true //true ou false
+
+const numeros : number = 10 //inteiro, floats, doubles, hexadecimal, octal
+
+console.log(nomeCompleto)
+console.log(nomeCompleto2)
