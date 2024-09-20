f911e2368653f99327017d456ef8763e6dc059b0
FerShibli <fernandoluiz.1991@gmail.com>
2024-09-19 21:59:24 -0300

    feat(header): add toggleMenu function to icone-usuario click event and click outside for deactivation

--- aula04-Angular/src/app/header/header.component.ts
+++ aula04-Angular/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,7 +8,38 @@ import { CommonModule } from '@angular/common';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
-  titulosite="Extensões Fatec";
-  menuusuario=false;
+export class HeaderComponent implements OnInit, OnDestroy {
+  titulosite = "Extensões Fatec";
+  menuusuario = false;
+  isBrowser: boolean;
+
+  constructor() {
+    this.isBrowser = typeof window !== 'undefined' && typeof document !== 'undefined';
+  }
+
+  ngOnInit() {
+    if (this.isBrowser) {
+      document.addEventListener('click', this.onDocumentClick.bind(this));
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.isBrowser) {
+      document.removeEventListener('click', this.onDocumentClick.bind(this));
+    }
+  }
+
+  toggleMenu() {
+    this.menuusuario = !this.menuusuario;
+  }
+
+  onDocumentClick(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    const menuElement = document.querySelector('.menu-usuario');
+    const userIconElement = document.querySelector('.icone-usuario');
+
+    if (menuElement && !menuElement.contains(target) && userIconElement && !userIconElement.contains(target)) {
+      this.menuusuario = false;
+    }
+  }
 }
