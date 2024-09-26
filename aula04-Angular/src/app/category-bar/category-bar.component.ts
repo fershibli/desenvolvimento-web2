340c5a95e565a579568f80dd0d481632230521d4
FerShibli <fernandoluiz.1991@gmail.com>
2024-09-26 20:53:40 -0300

    feat(category-bar): atualizar componente category-bar
    Adicionar a lista de categorias dinamicamente ao componente category-bar.

--- aula04-Angular/src/app/category-bar/category-bar.component.ts
+++ aula04-Angular/src/app/category-bar/category-bar.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { Category } from '../models/category';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-category-bar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './category-bar.component.html',
   styleUrl: './category-bar.component.css'
 })
 export class CategoryBarComponent {
-
+  categories: Category[] = [
+    { id: 1, image: 'assets/cursoti.jpg', title: 'Cursos de Ti' },
+    { id: 2, image: 'assets/cursovazio.png', title: 'Category 2' },
+    { id: 3, image: 'assets/cursovazio.png', title: 'Category 3' },
+    { id: 4, image: 'assets/cursovazio.png', title: 'Category 4' },
+    { id: 5, image: 'assets/cursovazio.png', title: 'Category 5' }
+  ];
 }
